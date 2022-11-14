c4165c264f4208e7ebcd83364a90d9f2a1bad2ae
Zita Koczka <koczka-z@ulster.ac.uk>
2022-11-14 00:58:23 +0000

    added UI to add movies activity

--- app/src/main/java/com/example/assignment2/AddMovies.java
+++ app/src/main/java/com/example/assignment2/AddMovies.java
@@ -3,6 +3,7 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class AddMovies extends AppCompatActivity {
 
@@ -12,5 +13,16 @@ public class AddMovies extends AppCompatActivity {
         setContentView(R.layout.activity_add_movies);
 
         getSupportActionBar().setTitle("");
+
+        EditText title = findViewById(R.id.add_et_title);
+        EditText year = findViewById(R.id.add_et_year);
+        EditText rated = findViewById(R.id.add_et_rated);
+        EditText released = findViewById(R.id.add_et_released);
+        EditText runtime = findViewById(R.id.add_et_runtime);
+        EditText genre = findViewById(R.id.add_et_genre);
+        EditText director = findViewById(R.id.add_et_director);
+        EditText writer = findViewById(R.id.add_et_writer);
+        EditText actors = findViewById(R.id.add_et_actors);
+        EditText plot = findViewById(R.id.add_et_plot);
     }
 }
