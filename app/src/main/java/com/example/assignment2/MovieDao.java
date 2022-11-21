3b92b86a2496c05e849e99fc0fdb4fcd5d109611
Zita Koczka <koczka-z@ulster.ac.uk>
2022-11-21 23:33:17 +0000

    added search for actors in local db functionality

--- app/src/main/java/com/example/assignment2/MovieDao.java
+++ app/src/main/java/com/example/assignment2/MovieDao.java
@@ -9,4 +9,7 @@ import java.util.List;
 @Dao
 public interface MovieDao {
     @Insert void addMovie(Movie movie);
+
+    @Query("SELECT * FROM movie WHERE actors LIKE '%' || :name || '%'")
+    List<Movie> getMoviesByActorName(String name);
 }
--- app/src/main/java/com/example/assignment2/SearchActors.java
+++ app/src/main/java/com/example/assignment2/SearchActors.java
@@ -3,8 +3,18 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 public class SearchActors extends AppCompatActivity {
+    MovieDatabase movieDatabase;
+    MovieDao movieDao;
+    EditText name_et;
+    TextView results;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +22,36 @@ public class SearchActors extends AppCompatActivity {
         setContentView(R.layout.activity_search_actors);
 
         getSupportActionBar().setTitle("");
+
+        movieDatabase = MovieDatabase.getMovieDatabase(getApplicationContext());
+        movieDao = movieDatabase.movieDao();
+
+        name_et = findViewById(R.id.searchactors_actors_et);
+        results = findViewById(R.id.searchactors_results);
+    }
+
+    public void searchOnClick(View view) {
+        String name = name_et.getText().toString();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                List<Movie> movies = movieDao.getMoviesByActorName(name);
+
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        String result = "";
+                        for (Movie movie : movies) {
+                            String row = movie.getTitle() + " - " + movie.getActors() + "\n";
+                            result += row;
+                        }
+                        results.setText(result);
+                        Toast.makeText(SearchActors.this, "count: " + movies.size(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).start();
+
     }
 }
