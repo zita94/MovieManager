6b4a1b9b6fda3116e6a5bc38f724c78deed26935
Zita Koczka <koczka-z@ulster.ac.uk>
2022-11-24 00:08:34 +0000

    added get by title substring from api functionality

--- app/src/main/java/com/example/assignment2/Advanced.java
+++ app/src/main/java/com/example/assignment2/Advanced.java
@@ -3,8 +3,25 @@ package com.example.assignment2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.SQLOutput;
 
 public class Advanced extends AppCompatActivity {
+    EditText titleEt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +29,43 @@ public class Advanced extends AppCompatActivity {
         setContentView(R.layout.activity_advanced);
 
         getSupportActionBar().setTitle("");
+
+        titleEt = findViewById(R.id.advanced_enterTitle);
+    }
+
+    public void searchOnClick(View view) {
+        String movieTitle = titleEt.getText().toString();
+        String url = "https://www.omdbapi.com/?s=*" + movieTitle + "*&apikey=" + getResources().getString(R.string.API_key);
+        getMovieData(url);
+    }
+
+    private void getMovieData(String url) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url,
+                null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    JSONArray results = response.getJSONArray("Search");
+                    for (int i = 0; i < results.length(); i++) {
+                        JSONObject movie = results.getJSONObject(i);
+                        String title = movie.getString("Title");
+                        System.out.println("Title: " + title);
+                    }
+
+                    String total = response.getString("totalResults");
+                    System.out.println("Total " + total);
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Volley Error Response", error.getMessage(), error);
+            }
+        });
+        queue.add(jsonObjectRequest);
     }
 }
